d5d27cac28ab7f12670bca841a00ec2f3ebac429
AnshArnavXalxo <84766585+AnshArnavXalxo@users.noreply.github.com>
2021-08-26 17:35:39 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -22,7 +22,7 @@ var zombieImgSad;
 
 function preload(){
 backgroundImg= loadImage("assets/background.png");
-zombieImg = loadImage("assets/zombie1.png");
+zombieImg = loadImage("assets/zombie3.png");
 zombieImgSad = loadImage("assets/sad_zombie.png");
 }
 
@@ -42,9 +42,9 @@ button= createImg("assets/axe.png");
 button.position(1200,310);
 button.size(50,50);
 button.mouseClicked(drop);
-zombie = createSprite(500,580,100,150);
+zombie = createSprite(460,565,100,150);
   zombie.addImage(zombieImg);
-  zombie.scale=0.15;
+  zombie.scale=0.09;
 
   zombie.addImage('Sad',zombieImgSad);
 }
@@ -55,9 +55,17 @@ function draw() {
 
   image(backgroundImg,0, 0,1400,660);
 
-  if(Collide(stone,zombie)){
-    zombie.changeImage("Sad")
+  for(var stone of stones){
+    stone.display();
+    var pos=stone.body.position;
+    var distance= dist(zombie.position.x,zombie.position.y,pos.x,pos.y);
+    if(distance<=50){
+      zombie.velocityX=0;
+      Matter.Body.setVelocity(stone.body,{x:10,y:-10});
+      zombie.changeImage("Sad");
+      collided= true;
     }
+  }
 
   ground.show();
   fill("black");
@@ -73,7 +81,7 @@ function showStones(){
     if(stones.length<8 && stones[stones.length-1].body.position.x<width-200){
     var pos=[-90,-120,-100,-80];
     var ypos=random(pos)
-    var stone= new Stone(width-800,height-620,40,ypos)
+    var stone= new Stone(width-800,height-700,40,ypos)
     stones.push(stone);
       }
       for(var i=0;i<stones.length;i++){
@@ -81,7 +89,7 @@ function showStones(){
       }
   }
   else{
-    var stone= new Stone(width-1000,height-620,40,-80)
+    var stone= new Stone(width-1000,height-700,40,-80)
     stones.push(stone);
   }
 }
